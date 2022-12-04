c93b581e6a618764e54536f2ca79c7de160c06e4
Noverdi Ramadhan <noverdy123@gmail.com>
2022-12-05 00:54:26 +0700

    add Profile* Activity

--- app/src/main/java/com/fordaku/ProfileActivity.kt
+++ app/src/main/java/com/fordaku/ProfileActivity.kt
@@ -2,11 +2,16 @@ package com.fordaku
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<ImageView>(R.id.backImageView).setOnClickListener {
+            finish()
+        }
     }
 }
