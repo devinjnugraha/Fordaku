232af8e34707f2df0372a03349205b8a4389491f
Noverdi Ramadhan <noverdy123@gmail.com>
2022-12-07 01:09:22 +0700

    refactoring

--- app/src/main/java/com/fordaku/fragments/BerandaFragment.kt
+++ app/src/main/java/com/fordaku/fragments/BerandaFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
@@ -19,6 +20,8 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import com.google.firebase.ktx.Firebase
+import java.text.SimpleDateFormat
+import java.util.*
 
 class BerandaFragment : Fragment() {
     private lateinit var mAdapter: FirestoreRecyclerAdapter<Posts, PostAdapter.PostsViewHolder>
@@ -57,6 +60,14 @@ class BerandaFragment : Fragment() {
         if (Firebase.auth.currentUser == null) {
             postButton.visibility = View.GONE
         }
+        view.findViewById<TextView>(R.id.dateTimeTextView).text = SimpleDateFormat("EEEE, dd MMMM yyyy").format(Date())
+
+        val time = SimpleDateFormat("HH").format(Date()).toInt()
+        val status = if (time in 5..10) "Pagi"
+            else if (time in 10..15) "Siang"
+            else if (time in 15..18) "Sore"
+            else "Malam"
+        view.findViewById<TextView>(R.id.dayTextView).text = "Selamat $status!"
     }
 
     override fun onStart() {
